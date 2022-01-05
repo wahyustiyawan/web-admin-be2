8868624f86c8d81fd0c7b2f2b9fc9a9420df7eec
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2022-01-05 11:38:23 +0700

    revisi quiz

--- app/Http/Resources/QuestionResource.php
+++ app/Http/Resources/QuestionResource.php
@@ -13,18 +13,21 @@ class QuestionResource extends JsonResource
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
-    {   $opsi = ["opsi_a"=> $this->opsi_a,
-        "opsi_b"=> $this->opsi_b,
-        "opsi_c"=> $this->opsi_c,
-        "opsi_d"=> $this->opsi_d,
-        "opsi_e"=> $this->opsi_e,];
+    {
+        $opsi = [
+            $this->opsi_a,
+            $this->opsi_b,
+            $this->opsi_c,
+            $this->opsi_d,
+            $this->opsi_e,
+        ];
         return [
             'id' => $this->id,
-            "no"=> $this->no,
-            "pertanyaan"=> $this->soal,
+            "no" => $this->no,
+            "pertanyaan" => $this->soal,
             "opsi" => $opsi,
-            "jawaban"=> $this->jawaban,
-            "quiz_id"=> $this->quiz_id,
+            "jawaban" => $this->jawaban,
+            "quiz_id" => $this->quiz_id,
             //'video' => $this->get_video,
         ];
     }
