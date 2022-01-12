2939ecde174dedb27d6f8ce08a75e4a3295638dd
Riyanti Maulya <riyantimaulya17@gmail.com>
2022-01-12 11:45:31 +0700

    up

---
+++ database/seeders/EnrollsSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\EnrollMataKuliah;
+use App\Models\EnrollStudi;
+use Illuminate\Database\Seeder;
+use App\Models\MataKuliah;
+
+class EnrollsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        EnrollStudi::create([
+            'id' => 1,
+            'user_id' => 4,
+            'kelas_id' => 1
+        ]);
+
+        EnrollMataKuliah::create([
+            'id' => 1,
+            'user_id' => 4,
+            'mata_kuliah_id' => 1,
+            'enroll_studi_id' => 1,
+        ]);
+        EnrollMataKuliah::create([
+            'id' => 2,
+            'user_id' => 5,
+            'mata_kuliah_id' => 1,
+            'enroll_studi_id' => 1,
+        ]);
+        EnrollMataKuliah::create([
+            'id' => 3,
+            'user_id' => 6,
+            'mata_kuliah_id' => 1,
+            'enroll_studi_id' => 1,
+        ]);
+    }
+}
+
