9990a73d4be58f9772fbb8a5fa5779d4e668fc43
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2022-01-17 10:17:46 +0700

    Helper

--- config/app.php
+++ config/app.php
@@ -210,6 +210,7 @@ return [
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
+        'Helper'=> App\Helpers\Helper::class,
         'Http' => Illuminate\Support\Facades\Http::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
