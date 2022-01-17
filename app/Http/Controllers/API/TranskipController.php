9a57e88731c5417a94ce218ce6e8c7562ee07e88
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2022-01-17 20:42:00 +0700

    up

--- app/Http/Controllers/API/TranskipController.php
+++ app/Http/Controllers/API/TranskipController.php
@@ -13,14 +13,20 @@ class TranskipController extends Controller
 {
     public function index($id)
     {
-        //Nilai dan semester where user tersebut
-        $transkip = EnrollMataKuliah::select('semester', DB::raw('SUM(nilai_akhir) as nilai'))->groupBy('semester')->where('user_id', $id)->get();
+        //Nilai dan semester where user tersebut || Output Nilai Akhir per semester
+        $transkip = EnrollMataKuliah::select(DB::raw('SUM(nilai_akhir) as nilai'))->groupBy('semester')->where('user_id', $id)->get();
 
+        
         //SKS di Tabel Mata Kuliah, kalau bisa ditambah langsung/Deteksi Persemester
         
 
+        // Ngambil jumlah sks yang diambil mahasiswa tersebut disemester itu
         //Total SKS Mahasiswa Dalam Semester Itu
-        $sks = MataKuliah::all();
+        $sks = EnrollMataKuliah::select('mata_kuliah.sks','enroll_mata_kuliah.semester','enroll_mata_kuliah.nilai_akhir', )->join('mata_kuliah','mata_kuliah.id','=','enroll_mata_kuliah.mata_kuliah_id')->where('user_id',$id)->get();
+       
+        foreach($sks as $item){
+             
+        }
 
         dd($transkip);
 
