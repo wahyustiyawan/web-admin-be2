87aa065cec466dccb09841e372edd23df0a42fb5
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2022-01-18 06:34:02 +0700

    transkip

--- app/Http/Controllers/API/TranskipController.php
+++ app/Http/Controllers/API/TranskipController.php
@@ -14,7 +14,7 @@ class TranskipController extends Controller
     public function index($id)
     {
         //Nilai dan semester where user tersebut || Output Nilai Akhir per semester
-        $transkip = EnrollMataKuliah::select(DB::raw('SUM(nilai_akhir) as nilai'))->groupBy('semester')->where('user_id', $id)->get();
+        $transkip = EnrollMataKuliah::select('semester',DB::raw('SUM(nilai_akhir) as nilai'))->groupBy('semester')->where('user_id', $id)->get();
 
         
         //SKS di Tabel Mata Kuliah, kalau bisa ditambah langsung/Deteksi Persemester
@@ -23,12 +23,29 @@ class TranskipController extends Controller
         // Ngambil jumlah sks yang diambil mahasiswa tersebut disemester itu
         //Total SKS Mahasiswa Dalam Semester Itu
         $sks = EnrollMataKuliah::select('mata_kuliah.sks','enroll_mata_kuliah.semester','enroll_mata_kuliah.nilai_akhir', )->join('mata_kuliah','mata_kuliah.id','=','enroll_mata_kuliah.mata_kuliah_id')->where('user_id',$id)->get();
-       
-        foreach($sks as $item){
-             
+        
+        $nilaiawal = 0;
+        $i = 0;
+        foreach($sks as $item){          
+
+            if($item->semester == $i){
+                $totalsks = $sks->where('semester',$item->semester)->SUM('sks');
+                $dataenroll = $sks->where('semester',$item->semester);
+                $nilai1 = $dataenroll->sks*$dataenroll->nilai_akhir;
+                $nilaiawal = $nilaiawal + $nilai1;
+
+                $data[] = [
+                    'semester' => $item->semester,
+                    'totalnilai'=>$nilaiawal,
+                    'totalsks' => $totalsks,
+                ];          
+                
+            
+            }
+            $i++;
         }
 
-        dd($transkip);
+        dd($totalsks);
 
         return response()->json([
             "error" => false,
