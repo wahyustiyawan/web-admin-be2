c3bcb85fb02eb44d06f841b0c4f68b7fc71c33c0
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2022-01-19 12:32:44 +0700

    Transkip overpower

--- app/Http/Controllers/API/TranskipController.php
+++ app/Http/Controllers/API/TranskipController.php
@@ -14,43 +14,53 @@ class TranskipController extends Controller
     public function index($id)
     {
         //Nilai dan semester where user tersebut || Output Nilai Akhir per semester
-        $transkip = EnrollMataKuliah::select('semester',DB::raw('SUM(nilai_akhir) as nilai'))->groupBy('semester')->where('user_id', $id)->get();
+        // $transkip = EnrollMataKuliah::select('semester',DB::raw('SUM(nilai_akhir) as nilai'))->groupBy('semester')->where('user_id', $id)->get();
 
-        
+        $transkip = EnrollMataKuliah::join('mata_kuliah', 'mata_kuliah.id', '=', 'enroll_mata_kuliah.mata_kuliah_id')->select('enroll_mata_kuliah.semester', DB::raw('SUM(mata_kuliah.sks) as jumlahsks'), DB::raw('SUM(enroll_mata_kuliah.nilai_akhir*mata_kuliah.sks) as nilai'))->groupBy('semester')->where('enroll_mata_kuliah.user_id', $id)->get();
+
         //SKS di Tabel Mata Kuliah, kalau bisa ditambah langsung/Deteksi Persemester
-        
+
 
         // Ngambil jumlah sks yang diambil mahasiswa tersebut disemester itu
         //Total SKS Mahasiswa Dalam Semester Itu
-        $sks = EnrollMataKuliah::select('mata_kuliah.sks','enroll_mata_kuliah.semester','enroll_mata_kuliah.nilai_akhir', )->join('mata_kuliah','mata_kuliah.id','=','enroll_mata_kuliah.mata_kuliah_id')->where('user_id',$id)->get();
-        
+        $sks = EnrollMataKuliah::select('mata_kuliah.sks', 'enroll_mata_kuliah.semester', 'enroll_mata_kuliah.nilai_akhir',)->join('mata_kuliah', 'mata_kuliah.id', '=', 'enroll_mata_kuliah.mata_kuliah_id')->where('user_id', $id)->get();
+
         $nilaiawal = 0;
         $i = 0;
-        foreach($sks as $item){          
+        // foreach($sks as $item){          
 
-            if($item->semester == $i){
-                $totalsks = $sks->where('semester',$item->semester)->SUM('sks');
-                $dataenroll = $sks->where('semester',$item->semester);
-                $nilai1 = $dataenroll->sks*$dataenroll->nilai_akhir;
-                $nilaiawal = $nilaiawal + $nilai1;
+        //     if($item->semester == $i){
+        //         $totalsks = $sks->where('semester',$item->semester);
+        //         $dataenroll = $sks->where('semester',$item->semester);
+        //         $nilai1 = $dataenroll->sks*$dataenroll->nilai_akhir;
+        //         $nilaiawal = $nilaiawal + $nilai1;
 
-                $data[] = [
-                    'semester' => $item->semester,
-                    'totalnilai'=>$nilaiawal,
-                    'totalsks' => $totalsks,
-                ];          
-                
-            
-            }
-            $i++;
+        //         $data[] = [
+        //             'semester' => $item->semester,
+        //             'totalnilai'=>$nilaiawal,
+        //             'totalsks' => $totalsks,
+        //         ];          
+
+
+        //     }
+        //     $i++;
+        // }
+
+        foreach($transkip as $item){
+            $data[] = [
+                'SKS' => (int)$item->jumlahsks,
+                // 'IPS' => (int)$item->nilai/$item->jumlahsks,
+                'IPS' => (int)($item->nilai/$item->jumlahsks)/25,
+                'Semester' => $item->semester,
+            ];
         }
 
-        dd($totalsks);
+        // dd($transkip);
 
         return response()->json([
             "error" => false,
             "message" => "Success",
-            "data" => $transkip
+            "data" => $data
         ], 200);
     }
 }
