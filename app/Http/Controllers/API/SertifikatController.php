eb187d896b6ef9721702e9076b386c42427896cf
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-01-24 15:38:19 +0700

    push

--- app/Http/Controllers/API/SertifikatController.php
+++ app/Http/Controllers/API/SertifikatController.php
@@ -7,31 +7,34 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Models\User;
-use Image;
+use Intervention\Image\Facades\Image;
 
 
 class SertifikatController extends Controller
 {
-    public function sertifikat(Request $request)  
+    public function sertifikat()  
     { 
-       $host = $request->getSchemeAndHttpHost(); 
+    //    $host = $request->getSchemeAndHttpHost(); 
        $user = Auth::user();
-       $img = Image::make(public_path('sertifikat/sertifikat.jpg'));  
-       $img->text(strval($user->name), 120, 100, function() {  
-        //   $font->file(public_path('path/font.ttf'));  
-        //   $font->size(28);  
-        //   $font->color('#e1e1e1');  
-        //   $font->align('center');  
-        //   $font->valign('bottom');  
-        //   $font->angle(90);  
-      });  
-       $img->save(public_path('sertifikat/sertifikat.jpg'));
+       $img = Image::make('sertifikat/sertifikat.jpg');  
+       $img->text(strval($user->name), 120, 100, function($font) {  
+        $font->file('font/roboto.regular.ttf');  
+        $font->size(28);  
+        $font->color('#e1e1e1');  
+        $font->align('center');  
+        $font->valign('bottom');  
+        $font->angle(90);  
+    });  
+       $img->save('sertifikat/sertifikat.jpg');
        
-        return response()->json([
-            "error" => false,
-            "message" => "Success",
-            "data" => base64_encode($host.'/'.$img)
-        ], 200);
+        // return response()->json([
+        //     "error" => false,
+        //     "message" => "Success",
+        //     "data" => base64_encode($host.'/'.$img)
+        // ], 200);
+
+        print($img);
+        return redirect($img);
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -211,7 +211,7 @@ Route::get('/jumlah-enroll-matkul/{id}', [EnrollMataKuliahController::class, 'en
 
 //Route Administrasai
 Route::post('/administrasi', [AdministrationController::class, 'store']);
-// Route::get('/sertifikat/{id}', [SertifikatController::class, 'sertifikat']);    
+// Route::get('/sertifikat', [SertifikatController::class, 'sertifikat']);    
 
 // Protected routes
 Route::group(['middleware' => ['auth:api']], function () {
