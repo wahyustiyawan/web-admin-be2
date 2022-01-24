9e91b06f9f87dff16c605e0ce4ec26bd8f0e916b
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-01-24 13:02:16 +0700

    push

--- app/Http/Controllers/API/SertifikatController.php
+++ app/Http/Controllers/API/SertifikatController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Models\User;
 use Image;
 
 
@@ -15,7 +17,7 @@ class SertifikatController extends Controller
        $host = $request->getSchemeAndHttpHost(); 
        $user = Auth::user();
        $img = Image::make(public_path('sertifikat/sertifikat.jpg'));  
-       $img->text($user->name, 120, 100, function() {  
+       $img->text(strval($user->name), 120, 100, function() {  
         //   $font->file(public_path('path/font.ttf'));  
         //   $font->size(28);  
         //   $font->color('#e1e1e1');  
@@ -28,7 +30,7 @@ class SertifikatController extends Controller
         return response()->json([
             "error" => false,
             "message" => "Success",
-            "data" => $host.'/'.$img
+            "data" => base64_encode($host.'/'.$img)
         ], 200);
     }
 
