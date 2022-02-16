2f2d64ab18c27a0ae823ba450e6373fffa5a0e75
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-02-16 10:21:27 +0700

    push

--- app/Http/Controllers/API/AdministrationController.php
+++ app/Http/Controllers/API/AdministrationController.php
@@ -178,7 +178,7 @@ class AdministrationController extends Controller
         //     'surat_rekomendasi' => $upload_surat_rekomendasi,
         //     'program' => $request->program,
         // );
-        $administrasi = Administration::where('user_id', $request->user->id)->update([
+        $administrasi = Administration::where('user_id', $user->id)->update([
             'user_id' => $user->id,
             'nama_lengkap' => $request->nama_lengkap,
             'nik' => $request->nik,
